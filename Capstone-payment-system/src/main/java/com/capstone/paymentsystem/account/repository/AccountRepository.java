a5285a0278985eccbdeec3dda6ef6b509fb420a9
Avinash Kumar Yadav <74833998+Ykavinash@users.noreply.github.com>
2022-04-12 16:34:40 +0530

    Delete Capstone-payment-system directory

--- Capstone-payment-system/src/main/java/com/capstone/paymentsystem/account/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capstone.paymentsystem.account.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capstone.paymentsystem.account.dto.Accounts;
-
-public interface AccountRepository extends JpaRepository<Accounts,String>  {
-
-}
--- Capstone-payment-system/src/main/java/com/capstone/paymentsystem/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capstone.paymentsystem.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capstone.paymentsystem.user.dto.Users;
-
-public interface UserRepository extends JpaRepository<Users, String> {
-
-}
